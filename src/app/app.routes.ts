96702ecc9fd8cad32ace53d0ba655b36cff06599
Bruno <brunobinfo@gmail.com>
2018-10-19 19:21:50 -0300

    Create components Menu, Restaurant Detail and Shopping Card

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantDetailComponent } from './restaurants/restaurant-detail/restaurant-detail.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { Routes } from '@angular/router';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'about', component: AboutComponent },
-    { path: 'restaurants', component: RestaurantsComponent }
+    { path: 'restaurants', component: RestaurantsComponent },
+    { path: 'restaurants/:id', component: RestaurantDetailComponent },
+    { path: 'about', component: AboutComponent }
 ]
